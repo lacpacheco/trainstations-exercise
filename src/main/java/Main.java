a5d603c5070ff4569a2c0db4469734c7c5420b05
lacpacheco <lacpacheco@gmail.com>
2016-01-07 15:10:36 +0000

    Demo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -35,7 +35,7 @@ public class Main {
       Map<String, Object> attributes = new HashMap<>();
       attributes.put("message", "Hello World!");
 
-      return new ModelAndView(attributes, "index.ftl");
+      return new ModelAndView(attributes, "pathFinder.ftl");
     }, new FreeMarkerEngine());
 
     get("/", (request, response) -> {
